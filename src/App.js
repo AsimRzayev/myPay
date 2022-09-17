22ff6943b6e199a582f7abed58080ceb000b9050
AsimRzayev <asimrr@code.edu.az>
2022-09-17 14:11:04 +0400

    Change text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          SALAM <code>src/App.js</code> and save to reload.
+          Sagol <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
