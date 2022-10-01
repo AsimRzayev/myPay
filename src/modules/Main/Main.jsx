5f94ff47bf2d863fae258d9a28e8b2db4c8ee7bb
AsimRzayev <83570091+AsimRzayev@users.noreply.github.com>
2022-10-01 14:36:08 +0400

    Revert "Revert "addBanner""

--- src/modules/Main/Main.jsx
+++ src/modules/Main/Main.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+import Banner from "../../components/Banner/Banner";
+
 export default function Main() {
-  return <div>Main</div>;
+  return (
+    <div>
+      <Banner desc="Repellat deserunt eos vero est necessitatibus quia. Repellendus voluptatem reprehenderit maiores et. Velit vero accusantium quaerat tempore animi porro."></Banner>
+    </div>
+  );
 }
