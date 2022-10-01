dbd228a8e6dfeb7d5fadd8ae39004fd92f7dfec7
Kenan Talibli <k.talibli007@gmail.com>
2022-10-01 14:33:34 +0400

    add

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,6 +1,4 @@
-import "../Banner/Banner.css";
-
-// import { Box } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Img, Text } from "@chakra-ui/react";
 import React from "react";
 
 //** Images */
@@ -9,40 +7,135 @@ import inputImg from "./images/input.svg";
 import servicesCard from "./images/services.svg";
 import signIn from "./images/sign-in.svg";
 
-export default function Banner() {
+export default function Banner({ desc }) {
   return (
     <>
-      <div className="Banner-container">
-        <div className="Banner">
-          <div className="Banner-left">
-            <h2>
-              Online ödəniş <span>etməyin ən asan yolu</span>
-            </h2>
-            <p>
-              Repellat deserunt eos vero est necessitatibus quia. Repellendus
-              voluptatem reprehenderit maiores et. Velit vero accusantium
-              quaerat tempore animi porro.
-            </p>
+      <Flex
+        className="Banner-container"
+        w={"1512px"}
+        h={"608px"}
+        p={"80px 10px 80px 96px"}
+        flexDirection={"column"}
+        justifyContent={"center"}
+        alignItems={"flex-start"}
+        gap={"4px"}
+      >
+        <Flex
+          className="Banner"
+          w={"1406px"}
+          h={"448px"}
+          flexDirection={"row"}
+          alignItems={"center"}
+          p={"0px"}
+          gap={"105px"}
+        >
+          <Flex
+            className="Banner-left"
+            w={"529px"}
+            h={"358px"}
+            flexDirection={"column"}
+            alignItems={"flex-start"}
+            p={"0px"}
+            position={"relative"}
+          >
+            <Heading
+              as={"h2"}
+              fontWeight={"700"}
+              fontStyle={"normal"}
+              fontFamily={"Montserrat"}
+              w={"536px"}
+              h={"112px"}
+              fontSize={"40px"}
+              lineHeight={"56px"}
+              color={"#44bc82"}
+            >
+              Online ödəniş{" "}
+              <Text as={"span"} color={"#586268"}>
+                etməyin ən asan yolu
+              </Text>
+            </Heading>
+            <Text
+              as={"p"}
+              w={"536px"}
+              h={"48px"}
+              fontStyle={"normal"}
+              fontWeight={"400"}
+              fontSize={"14px"}
+              lineHeight={"24px"}
+              mt={"20px"}
+              color={"#586268"}
+            >
+              {desc}
+            </Text>
 
-            <button>
-              <img src={signIn} alt="sign-in" />
+            <Button
+              h={"48px"}
+              w={"137px"}
+              borderRadius={"8px"}
+              p={"16px 20px 16px 20px"}
+              display={"flex"}
+              flexDirection={"row"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              position={"absolute"}
+              left={"-7.08px"}
+              bottom={"0%"}
+              backgroundColor={"#0d52ff"}
+              color={"#f8fafc"}
+              boxShadow={"0px 4px 19px rgba(48, 46, 150, 0.25)"}
+              transition={"all"}
+              transitionDuration={"0.2s"}
+              transitionTimingFunction={"ease"}
+              _hover={{
+                bgColor: "#110cdf",
+              }}
+            >
+              <Img src={signIn} alt="sign-in" fontSize={"14px"} mr={"8px"} />
               İndi başla
-            </button>
-          </div>
-          <div className="Banner-right">
-            <div className="services-img">
-              <img src={servicesCard} alt="Services image" />
-            </div>
-            <div className="card-img">
-              <img src={imageCard} alt="Card image" />
-            </div>
-            <div className="input-img">
-              {" "}
-              <img src={inputImg} alt="Search" />{" "}
-            </div>
-          </div>
-        </div>
-      </div>
+            </Button>
+          </Flex>
+
+          <Box
+            className="Banner-right"
+            w={"772px"}
+            h={"448px"}
+            position={"relative"}
+          >
+            <Box>
+              <Img
+                src={servicesCard}
+                alt="Services image"
+                className="services-img"
+                position={"absolute"}
+                right={"398px"}
+                bottom={"-43px"}
+                filter={"drop-shadow(0px 4px 60px rgba(54, 64, 224, 0.08))"}
+              />
+            </Box>
+            <Box className="card-img">
+              <Img
+                src={imageCard}
+                alt="Card image"
+                position={"absolute"}
+                top={"4px"}
+                left={"136px"}
+                filter={"drop-shadow(0px 4px 60px rgba(54, 64, 224, 0.08))"}
+                zIndex={"2"}
+              />
+            </Box>
+            <Box className="input-img">
+              <Img
+                src={inputImg}
+                alt="Search"
+                position={"absolute"}
+                right={"-10px"}
+                bottom={"-60px"}
+                filter={"drop-shadow(0px 4px 60px rgba(54, 64, 224, 0.08))"}
+              />
+            </Box>
+          </Box>
+        </Flex>
+      </Flex>
     </>
   );
 }
--- src/modules/Main/Main.jsx
+++ src/modules/Main/Main.jsx
@@ -5,7 +5,7 @@ import Banner from "../../components/Banner/Banner";
 export default function Main() {
   return (
     <div>
-      <Banner></Banner>
+      <Banner desc="Repellat deserunt eos vero est necessitatibus quia. Repellendus voluptatem reprehenderit maiores et. Velit vero accusantium quaerat tempore animi porro."></Banner>
     </div>
   );
 }
